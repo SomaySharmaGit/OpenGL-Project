187b198d4f0914d51a1fae072b5c4cdab2ce6965
SomaySharmaGit <somay052407@gmail.com>
2026-04-08 21:27:54 -0400

    Added: Data neeede to make an OpenCL program

--- main.cpp
+++ main.cpp
@@ -8,8 +8,6 @@
 #include <math.h>
 #include "SHADER.h"
 
-
-
 int main(){
 
     int err;
@@ -17,24 +15,39 @@ int main(){
     cl_platform_id platform_id;
     cl_device_id device_id;
 
-
+    cl_context context;
+    cl_command_queue commands;
+    cl_program program;
+        
+    //platform binding
     err = clGetPlatformIDs(1, &platform_id, NULL);
     err = clGetDeviceIDs(platform_id, CL_DEVICE_TYPE_GPU, 1, &device_id, NULL);
     if(err != CL_SUCCESS)
     {
         printf("Error: Failed to create a device group!\n");
         return 1;
-    }   
+    }
+
+    //compute context
+    context = clCreateContext(0,1, &device_id, NULL, NULL, &err);
+    if(!context)
+    {
+        printf("Error: Failed to create a context");
+        return 1;
+    }
+
+    //command list
+    commands = clCreateCommandQueue(context, device_id, 0, &err);
+    if(!commands)
+    {
+        printf("Error: Failed to create command queue!\n");
+        return 1;
+    }
+
+    //program
     
-    size_t device_message_size;
-    err = clGetDeviceInfo(device_id, CL_DEVICE_NAME, 0, NULL, &device_message_size);
 
-    char *message = new char[device_message_size];
-    err = clGetDeviceInfo(device_id, CL_DEVICE_NAME, device_message_size, message, NULL);
 
-    printf(message);
-
-    delete[] message;
 
     return 0;
 }
